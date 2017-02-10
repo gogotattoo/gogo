8e0ba0149d7934de8ca654cbfeae8e4f29da29c3
Roman <roman@tronenko.net>
2017-02-10 16:11:52 +0800

    fade in on image load - can be improved

--- static/js/gogo.js
+++ static/js/gogo.js
@@ -17,6 +17,7 @@
      return this;
  };
 
+
  function zoomImage()
  {
    // There's a small disagreement in the community if we need this funcion or not,
@@ -82,4 +83,19 @@
     $('ul.tags li').hide().slice(0, 9).show();
     $('ul.tags').fadeIn().css("display","inline-block");
 
+    $(".fadeIn").each(function() {
+         var src = $(this).data("src");
+         if (src) {
+             var img = new Image();
+             var card = $(this).parent().parent().parent();
+             img.style.display = "none";
+             img.onload = function() {
+                 $(this).fadeIn(1000);
+                 card.fadeIn(500);
+             };
+             $(this).append(img);
+             img.src = src;
+
+         }
+     });
 })(jQuery); // End of use strict
